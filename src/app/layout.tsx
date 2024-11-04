bf98682dee2ef6e12c9476bde090525c69618278
CROSS9111 <hirotoshikurosu@gmail.com>
2024-11-04 23:12:57 +0900

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from 'react'
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../../components/layouts/Header/Header";
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Suspense>
         <Header />
+        </Suspense>
         {/* <AuthProvider> */}
         {children}
         {/* </AuthProvider> */}
