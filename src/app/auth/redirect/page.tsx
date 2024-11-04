b6c0ce95578fc017f803982f2f0635432ccdebd5
CROSS9111 <hirotoshikurosu@gmail.com>
2024-11-04 22:28:37 +0900

    add page

--- src/app/auth/redirect/page.tsx
+++ src/app/auth/redirect/page.tsx
@@ -1,36 +1,74 @@
+// 'use client';
+// import { AuthenticationResult } from "@azure/msal-node"
+// import axios from "axios"
+// import { useSearchParams } from "next/navigation"
+// import { useEffect, useState, Suspense } from "react"
+
+// export default function Home() {
+//   const params = useSearchParams()
+//   const [state, setState] = useState({
+//     jwt: "",
+//   })
+//   // const [code, _] = useState(params.get("code"))
+//   const [code, _] = useState(params ? params.get("code") : null);
+
+
+//   useEffect(() => {
+//     if (!code) return; // codeがnullの場合はリクエストを行わない
+
+//     (async() => {
+//       // 認証をかける
+//       const url = "/api/auth/verify"
+//       const {data}: {data: AuthenticationResult} = await axios.post(url, {
+//         code
+//       })
+//       setState({jwt: data.accessToken})
+//     })()
+//   }, [code])
+
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//     <div>
+//       {state.jwt}
+//     </div>
+//     </Suspense>
+//   )
+// }
+
 'use client';
-import { AuthenticationResult } from "@azure/msal-node"
-import axios from "axios"
-import { useSearchParams } from "next/navigation"
-import { useEffect, useState, Suspense } from "react"
+import { AuthenticationResult } from "@azure/msal-node";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState, Suspense } from "react";
 
 export default function Home() {
-  const params = useSearchParams()
+  const params = useSearchParams();
   const [state, setState] = useState({
     jwt: "",
-  })
-  // const [code, _] = useState(params.get("code"))
-  const [code, _] = useState(params ? params.get("code") : null);
-
+  });
 
   useEffect(() => {
+    const code = params ? params.get("code") : null;
     if (!code) return; // codeがnullの場合はリクエストを行わない
 
-    (async() => {
-      // 認証をかける
-      const url = "/api/auth/verify"
-      const {data}: {data: AuthenticationResult} = await axios.post(url, {
-        code
-      })
-      setState({jwt: data.accessToken})
-    })()
-  }, [code])
+    (async () => {
+      try {
+        const url = "/api/auth/verify";
+        const { data }: { data: AuthenticationResult } = await axios.post(url, {
+          code,
+        });
+        setState({ jwt: data.accessToken });
+      } catch (error) {
+        console.error("Error during authentication", error);
+      }
+    })();
+  }, [params]);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-    <div>
-      {state.jwt}
-    </div>
+      <div>
+        {state.jwt ? <div>JWT: {state.jwt}</div> : <div>Authenticating...</div>}
+      </div>
     </Suspense>
-  )
+  );
 }
